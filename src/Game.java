949ead88f675839bcfe9e53997c9c55e2c6a1857
Lovinder Pnag <lspnag@gmail.com>
2016-03-02 03:17:44 -0500

    passing test for one spare

--- src/Game.java
+++ src/Game.java
@@ -12,8 +12,13 @@ public class Game {
         int score = 0;
         int frameIndex = 0;
         for(int frame = 0; frame < 10; frame++) {
-            score += rolls[frameIndex] + rolls[frameIndex +1];
-            frameIndex += 2;
+            if(rolls[frameIndex] + rolls[frameIndex +1] == 10) {
+                score += 10 + rolls[frameIndex +2];
+                frameIndex += 2;
+            } else {
+                score += rolls[frameIndex] + rolls[frameIndex +1];
+                frameIndex += 2;
+            }
         }
         return score;
     }
