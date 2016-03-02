9bfe223218592708d6daadead76a0b133fdb6796
Lovinder Pnag <lspnag@gmail.com>
2016-03-02 02:32:19 -0500

    first test for gutter Game passing

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,10 +1,14 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class BowlingGameTest {
 
     @Test
     public void testGutterGame() {
         Game g = new Game();
-
+        for(int i = 0; i < 20; i++)
+            g.roll(0);
+        assertEquals(0, g.score());
     }
 }
