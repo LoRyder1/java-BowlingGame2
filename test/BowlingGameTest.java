38e80ab2e9c95c0a89ff375c424f76923f7984f4
Lovinder Pnag <lspnag@gmail.com>
2016-03-02 03:24:36 -0500

    passing test for one strike

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -46,4 +46,13 @@ public class BowlingGameTest {
         rollMany(17, 0);
         assertEquals(16, g.score());
     }
+
+    @Test
+    public void testOneStrike() {
+        g.roll(10);
+        g.roll(3);
+        g.roll(4);
+        rollMany(16,0);
+        assertEquals(24, g.score());
+    }
 }
