d28768ddef9fe3fa4402a2b11583d49c704415f9
Lovinder Pnag <lspnag@gmail.com>
2016-03-02 02:40:51 -0500

    reformat and passing test for all ones

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,14 +1,28 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class BowlingGameTest {
 
+    private Game g;
+
+    @Before
+    public void setUp() {
+        g = new Game();
+    }
+
     @Test
     public void testGutterGame() {
-        Game g = new Game();
         for(int i = 0; i < 20; i++)
             g.roll(0);
         assertEquals(0, g.score());
+    }
+
+    @Test
+    public void testAllOnes() {
+        for(int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
     }
 }
