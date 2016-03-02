9bda8dbff2983cb14484a54d8df751d379d2d9b8
Lovinder Pnag <lspnag@gmail.com>
2016-03-02 03:09:29 -0500

    refactor score method so it actually does the scoring

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,20 @@
 
 public class Game {
 
-    private int score = 0;
+    private int rolls[] = new int [21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for(int i = 0; i < rolls.length; i++) {
+            score += rolls[frameIndex];
+            frameIndex += 1;
+        }
         return score;
     }
 }
